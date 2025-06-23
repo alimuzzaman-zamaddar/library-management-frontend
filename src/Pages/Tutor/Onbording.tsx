91c27c8927a7d545b0b8b59167a131971f980bd9
sultan93291 <abibdipto@gmail.com>
2025-06-23 19:09:44 +0600

    feat: fixed build error

--- src/Pages/Tutor/Onbording.tsx
+++ src/Pages/Tutor/Onbording.tsx
@@ -1,5 +1,4 @@
-// pages/onboarding.tsx
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import { useForm, FormProvider } from 'react-hook-form';
 import { TutorSignUpLayout } from './TutorSignUpLayout';
 
