7289b3885b81e731cd2345f5789699a17ce7fb57
alimuzzaman-zamaddar <alimuzzamanzamaddar@gmail.com>
2025-06-26 11:07:17 +0600

    done

--- src/Pages/Auth/Tutor/TutorSignUpLayout.tsx
+++ src/Pages/Auth/Tutor/TutorSignUpLayout.tsx
@@ -40,8 +40,8 @@ export const TutorSignUpLayout: React.FC<TutorSignUpLayoutProps> = ({
       <div className="w-full max-w-3xl bg-white p-8 border border-[#DBE1E5] rounded-lg shadow-md">
         {/* Title + Description */}
         <div className="mb-6">
-          <Heading Variant='h2' Txt={currentStep.title} className='text-[22px] xl:text-[32px] leading-[150%] font-bold text-gray-900 mb-4'  />
-          <Paragraph Txt={currentStep.description} className='text-sm text-gray-500'  />
+          <Heading data-aos="fade-up" Variant='h2' Txt={currentStep.title} className='text-[22px] xl:text-[32px] leading-[150%] font-bold text-gray-900 mb-4'  />
+          <Paragraph data-aos="fade-up" Txt={currentStep.description} className='text-sm text-gray-500'  />
 
         </div>
 
@@ -50,7 +50,7 @@ export const TutorSignUpLayout: React.FC<TutorSignUpLayoutProps> = ({
           <div className="text-sm text-gray-600 mb-2">
             Step {step} of {totalSteps}
           </div>
-          <div className="w-full h-2 bg-gray-200 rounded-full overflow-hidden">
+          <div data-aos="fade-up" className="w-full h-2 bg-gray-200 rounded-full overflow-hidden">
             <div
               className="h-2 bg-blue-900 rounded-full transition-all duration-300"
               style={{ width: `${progressPercent}%` }}
